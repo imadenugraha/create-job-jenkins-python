c5b8fdee9b27d192d8d80d1ea25879391d86cc16
imadenugraha <imadenugraha@gmail.com>
2024-04-09 23:39:21 +0700

    feat: add create job function

--- create_job.py
+++ create_job.py
@@ -1,3 +1,18 @@
 from jenkins_session import jenkins_session
+import logging
+import os
 
+def create_job() -> bool:
+    server = jenkins_session()
 
+    job_name = os.getenv('JOB_NAME')
+
+    with open('./Jenkins_Job_Config/config.xmlconfig.xml', 'r') as f:
+        job_config = f.read()
+
+    try:
+        server.create_job(job_name, job_config)
+        return True
+    except Exception as e:
+        logging.error(e)
+        return False
