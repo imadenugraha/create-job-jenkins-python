911d8dfd4ab73dfe414ee3df066df768a56512c0
imadenugraha <imadenugraha@gmail.com>
2024-04-10 15:04:50 +0700

    add: dotenv function

--- create_job.py
+++ create_job.py
@@ -1,14 +1,18 @@
+from dotenv import load_dotenv
 from jenkins_session import jenkins_session
 import logging
 import os
 
-def create_job() -> bool:
-    server = jenkins_session()
 
+def create_job() -> bool:
+    load_dotenv()
+
+    server = jenkins_session()
     job_name = os.getenv('JOB_NAME')
 
-    with open('./Jenkins_Job_Config/config.xmlconfig.xml', 'r') as f:
+    with open('./Jenkins_Job_Config/config.xml', 'r') as f:
         job_config = f.read()
+        f.close()
 
     try:
         server.create_job(job_name, job_config)
